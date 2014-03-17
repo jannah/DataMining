7156ac5e65d2b670da58c64085b89614b2b3a424
Shreyas <seekshreyas@gmail.com>
2014-03-16 20:39:05 -0700

    user similarity changes

--- solutions/hw3_mrjob/shreyas/user_similarity.py
+++ solutions/hw3_mrjob/shreyas/user_similarity.py
@@ -1,5 +1,12 @@
+###
+### This code was inspired by last year's student
+### Max Gutman
+###
+
+from __future__ import division
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
+from itertools import combinations
 
 
 class UserSimilarity(MRJob):
@@ -12,15 +19,51 @@ class UserSimilarity(MRJob):
     # reviewed businesses
     ##/
 
+    def extract(self, _, record):
+            """
+            Take in 1 record from JSON file and yield <user_id, business_id>
+            """
+            if record['type'] == 'review':
+                yield [record['user_id'], record['business_id']]
+
+
+    def combine(self, usr_id, biz_id):
+        yield [usr_id, list(set(biz_id))]
+
+
+    def distribute(self, usr_id, biz_id):
+        yield 'all', [usr_id, biz_id]
+
+
+
+    def similarity(self, _, usr_biz_pairs):
+        """
+        find similarity of users given they are aggregated as lists of
+        users and businesses
+        """
+        for (usr_a, biz_a), (usr_b, biz_b) in combinations(usr_biz_pairs, r=2):
+            sim = UserSimilarity.jaccard(biz_a, biz_b)
+
+            if sim >= 0.5:
+                yield [usr_a, usr_b], sim
+
+
+    ## Measure similarity
+    @staticmethod
+    def jaccard(biz_a, biz_b):
+        return float(len(set(biz_a) & set(biz_b))) / len(set(biz_a) | set(biz_b))
+
+
     def steps(self):
-        """TODO: Document what you expect each mapper and reducer to produce:
-        mapper1: <line, record> => <key, value>
-        reducer1: <key, [values]>
-        mapper2: ...
+        """
+        mapper1: <line, record> => <user_id, business_id>
+        reducer1: <user_id, business_id> => <user_id, [business_ids]>
+        mapper2: <user_id, [business_ids]> => <KEY, [user_id, business_ids]>
+        reducer2: <KEY, [user_id, business_ids]> => <[user_a, user_b], jaccard>
         """
         return [
-            self.mr(mapper=self.mapper1, reducer=self.reducer1),
-            # self.mr(mapper=...),
+            self.mr(mapper=self.extract, reducer=self.combine),
+            self.mr(mapper=self.distribute, reducer=self.similarity)
         ]
 
 
